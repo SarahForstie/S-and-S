d63cff5297c2cb2a93a14fd891028d9e28f7c378
SarahForstie <sarah.ih.forster@gmail.com>
2017-01-13 10:20:05 +0000

    Add files via upload

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,15 +1,18 @@
 import json
 import time
 import random
+import urllib.request
 
 def main():
-    day = time.strftime("%A")
-    login = loginGen(day)
-    print("login:",login)
-    password = getPassword(day)
-    print(password)
+    #runs the program.
+    day = time.strftime("%A") #called for day in main as multiple functions need it.
+    login = loginGen(day) #Creates the login.
+    print("login:",login) # To test.
+    password = getPassword(day) # Calls a function which calls other functions to put together the password.
+    print(password) # To test.
 
 def loginGen(dayname):
+    #Generates login as a list then joins the list and returns to main.
     login_list = []
     login_list.append(dayname)
     login_list.append(time.strftime("%#m"))
@@ -18,6 +21,7 @@ def loginGen(dayname):
     return login
 
 def getPassword(day):
+    # Acts as a hub for 4 functions to put together the password.
     word = getWord(day)
     character = genChar()
     number = genNum()
@@ -25,6 +29,7 @@ def getPassword(day):
     return password
     
 def getWord(dayname):
+    #Calls out to external API to get a word. The day name is used to slightly change the URL.
     RequestURL = "http://www.setgetgo.com/randomword/get.php?len="
     
     if dayname in ("Monday","Tuesday","Wednesday"):
@@ -35,26 +40,31 @@ def getWord(dayname):
 
     elif dayname in ("Saturday","Sunday"):
         RequestURL = RequestURL + "10"
+
+    response = urllib.request.urlopen(RequestURL)
+    myword = response.read().decode("utf-8")
         
-    return RequestURL
+    return myword.capitalize()
 
 def genChar():
+    #Randomly chooses from a set of characters then returns it.
     charlist = ["!","@","£","$","%","&","*","#"]
     character = random.choice(charlist)
     return character
 
 def genNum():
-    num = random.randint(1,9)
+    #Randomly generates a number and returns it.
+    num = random.randint(0,9)
     num = str(num)
     return num
 
 def makePassword(word, char, num):
+    #Puts together the password as a list then joins it and returns it.
     pass_list = []
     pass_list.append(word)
     pass_list.append(char)
     pass_list.append(num)
-    print(pass_list)
     password = "".join(pass_list)
     return password
 
-main()
+main() # Runs program
